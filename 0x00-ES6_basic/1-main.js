e094ed75fc1586c6368fc1ee4619201cfb4f578c
mosescode1 <emosescode@gmail.com>
2024-07-22 16:08:57 +0100

    done

---
+++ 0x00-ES6_basic/1-main.js
@@ -0,0 +1,5 @@
+#!/usr/bin/node
+import taskBlock from './1-block-scoped.js';
+
+console.log(taskBlock(true));
+console.log(taskBlock(false));
---
+++ 0x00-ES6_basic/4-rest-parameter.js
@@ -0,0 +1,5 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable */
+export default function returnHowManyArguments(...inputs) {
+    return inputs.length;
+}
