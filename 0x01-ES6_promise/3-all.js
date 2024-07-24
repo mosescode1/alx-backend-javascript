d89800d6c7cba59cff3b8733f5df116b91914e7f
mosescode1 <emosescode@gmail.com>
2024-07-24 16:24:26 +0100

    done

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -3,6 +3,6 @@ import { uploadPhoto, createUser } from './utils';
 export default function handleProfileSignup() {
   const allPromises = Promise.all([uploadPhoto(), createUser()]);
   allPromises
-    .then(([first, second]) => console.log(first.body, second.firstName, second.lastName))
+    .then(([first, second]) => console.log(`${first.body} ${second.firstName} ${second.lastName}`))
     .catch(() => console.error('Signup system offline'));
 }
