dc5189aa9cbb13f98fed52b338785ed07b811770
mosescode1 <emosescode@gmail.com>
2024-07-30 12:52:11 +0100

    reduce function

--- 0x03-ES6_data_manipulation/3-get_ids_sum.js
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -1,1 +1,6 @@
 #!/usr/bin/node
+/*eslint-disable*/
+export default function getStudentIdsSum(students = []) {
+	const id = students.map(student => student.id);
+	return id.reduce((acc, current) => acc + current, 0);
+}
