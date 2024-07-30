b2de27632bcab534ab7650ee14dc7107ceefbc02
mosescode1 <emosescode@gmail.com>
2024-07-30 16:57:39 +0100

    map

--- 0x03-ES6_data_manipulation/9-groceries_list.js
+++ 0x03-ES6_data_manipulation/9-groceries_list.js
@@ -1,1 +1,12 @@
 #!/usr/bin/node
+/*eslint-disable*/
+export default function groceriesList() {
+	const map = new Map();
+	map.set("Apples", 10);
+	map.set("Tomatoes", 10);
+	map.set("Pasta", 1);
+	map.set("Rice", 1);
+	map.set("Banana", 5);
+
+	return map;
+}
