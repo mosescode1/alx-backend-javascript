89cb24f075179e2e3f2ae755e8305b23704b5546
mosescode1 <emosescode@gmail.com>
2024-07-30 12:17:46 +0100

    using map

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,1 +1,9 @@
-#!/usr/bin/node
+/*eslint-disable*/
+export default function getListStudentIds(array) {
+	if (!Array.isArray(array)) {
+		return [];
+	}
+
+	const ids = array.map(elem => elem.id);
+	return ids;
+}
