0a52f5fa5533e8751736d91bda4ed21096e4cf67
mosescode1 <emosescode@gmail.com>
2024-07-30 15:44:34 +0100

    done

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,1 +1,14 @@
 #!/usr/bin/node
+/*eslint-disable*/
+export default function cleanSet(set, startString) {
+	if (startString === undefined || startString === '') return '';
+	const arr = [];
+	set.forEach(function (value) {
+		if (value.startsWith(startString)) {
+			const val = value.slice(startString.length);
+			arr.push(val);
+		}
+	})
+
+	return arr.join("-");
+}
