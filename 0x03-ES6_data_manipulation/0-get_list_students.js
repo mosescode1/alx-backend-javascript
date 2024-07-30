e03d2f9140fb7b2cdd9d0bfadb7c134fba0c58a8
mosescode1 <emosescode@gmail.com>
2024-07-30 12:08:10 +0100

    manipulating data

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,11 +1,11 @@
-#!/usr/bin/node
+/*eslint-disable*/
 export default function getListStudents() {
-  const obj1 = { id: 1, firstName: 'Guillaume', location: 'San Francisco' };
+	const obj1 = { id: 1, firstName: 'Guillaume', location: 'San Francisco' };
 
-  const obj2 = { id: 2, firstName: 'James', location: 'Columbia' };
-  const obj3 = {
-    id: 3, firstName: 'Serena', location: 'San Francisco',
-  };
+	const obj2 = { id: 2, firstName: 'James', location: 'Columbia' };
+	const obj3 = {
+		id: 3, firstName: 'Serena', location: 'San Francisco',
+	};
 
-  return [obj1, obj2, obj3];
+	return [obj1, obj2, obj3];
 }
