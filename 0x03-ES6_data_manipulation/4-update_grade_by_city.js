ebf096d807391039a5868492d9dec636eff001af
mosescode1 <emosescode@gmail.com>
2024-07-30 13:20:32 +0100

    all done

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,1 +1,13 @@
 #!/usr/bin/node
+/*eslint-disable*/
+export default function updateStudentGradeByCity(student, city, newGrades) {
+	return student.filter((elem) => elem.location === city)
+		.map((elem) => {
+			const grade = newGrades.find(grade => grade.id === elem.id);
+
+			return {
+				...elem,
+				grade: grade ? grade.grade : 'N/A'
+			}
+		})
+}
