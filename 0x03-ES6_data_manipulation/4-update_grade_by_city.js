e9e95a9fe2fa0af4281ded08eb532aa6401155a1
mosescode1 <emosescode@gmail.com>
2024-07-30 13:47:57 +0100

    all done

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -3,8 +3,8 @@
 export default function updateStudentGradeByCity(student, city, newGrades) {
 	return student.filter((elem) => elem.location === city)
 		.map((elem) => {
-			const grade = newGrades.find(grade => grade.id === elem.id);
-
+			const grade = newGrades.find(grade => grade.studentId === elem.id);
+			console.log("", grade);
 			return {
 				...elem,
 				grade: grade ? grade.grade : 'N/A'
--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -1,1 +1,13 @@
 #!/usr/bin/node
+/*eslint-disable*/
+export default function createInt8TypedArray(length, position, value) {
+	const buffer = new ArrayBuffer(length);
+
+	const dataView = new DataView(buffer);
+	if (position >= length || position < 0) {
+		throw new Error("Position outside range");
+	}
+	dataView.setUint8(position, value);
+
+	return dataView;
+}
