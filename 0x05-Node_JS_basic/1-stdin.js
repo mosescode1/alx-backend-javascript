a4a21ecb530a2549775ab18b88878ad276886bc0
mosescode1 <emosescode@gmail.com>
2024-09-18 09:50:15 +0100

    fixed linting error

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,7 +1,5 @@
 #!/usr/bin/node
-/**
- * Displays a message on the STDOUT.
- */
+// ! Using Process stdin
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
 process.stdin.on('data', function (data) {
