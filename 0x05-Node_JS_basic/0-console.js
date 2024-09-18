76cd24f30ce6abd152641b061da8ac40be5dd394
mosescode1 <emosescode@gmail.com>
2024-09-18 09:42:41 +0100

    refix task 0

--- 0x05-Node_JS_basic/0-console.js
+++ 0x05-Node_JS_basic/0-console.js
@@ -1,8 +1,6 @@
 #!/usr/bin/node
 // ! 0. Executing basic javascript with Node JS
-
 const displayMessage = (message) => {
-	process.stdout.write(message + "\n");
+	process.stdout.write(`${message}\n`);
 }
-
 module.exports = displayMessage;
