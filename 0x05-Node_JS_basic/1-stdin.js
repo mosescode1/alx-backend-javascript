c2a8887d0b8cb26651f78d502fb1c956b3cdf40b
mosescode1 <emosescode@gmail.com>
2024-09-18 09:49:39 +0100

    fixed linting error

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,11 +1,14 @@
 #!/usr/bin/node
-// ! Using Process stdin
+/**
+ * Displays a message on the STDOUT.
+ */
 process.stdout.write('Welcome to Holberton School, what is your name?\n');
+
 process.stdin.on('data', function (data) {
-  process.stdout.write(`Your name is: ${data.toString()}`);
-  process.exit();
+	process.stdout.write(`Your name is: ${data.toString()}`);
+	process.exit();
 });
 
 process.on('exit', function () {
-  process.stdout.write('This important software is now closing\n');
+	process.stdout.write('This important software is now closing\n');
 });
